1515a87b75b0cf92778a5ddcd72edad40cd9e567
momoogles <acmomoogles777@gmail.com>
2024-02-02 00:27:57 +0900

    relativeを忘れていたのとstacking contextを切る

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -53,7 +53,7 @@ export default function Layout({ children }: PropsWithChildren) {
       <div className="fixed right-0 top-0 grid h-[var(--header-height)] place-content-center pr-16">
         <a
           href={rpcClient.api.login.google.$url().toString()}
-          className="inline-grid h-40 place-content-center rounded-full bg-brand px-16 text-14 font-bold drop-shadow-4 hover:before:absolute hover:before:inset-0 hover:before:bg-surface3 hover:before:content-['']"
+          className="relative z-0 inline-grid h-40 place-content-center rounded-full bg-brand px-16 text-14 font-bold drop-shadow-4 hover:before:absolute hover:before:inset-0 hover:before:bg-surface3 hover:before:content-['']"
         >
           ログイン
         </a>
